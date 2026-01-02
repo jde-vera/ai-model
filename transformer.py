94bb27a8c13d7f6fb41c841d72a282931f66f79c
j <jdmichaelryan@gmail.com>
2026-01-02 22:51:34 +0000

    func: build_vocab(self, user_input) -> build vocab dict and their id

--- transformer.py
+++ transformer.py
@@ -1,8 +1,9 @@
 import re
 
 class Transformer:
-    
-    
+    def __init__(self):
+        self.vocab = {}
+
     def tokenize(self, user_input):
         '''
         the tokenize() take user input (string) and makes use of re
@@ -12,3 +13,23 @@ class Transformer:
             r"(?:[A-Za-z]+(?:[-'][A-Za-z]+)*) | (?:\d+(?:\.\d+)?) | (?:[^\w\s])", re.VERBOSE)
 
         return regex.findall(user_input)
+    
+    def build_vocab(self, user_input):
+        '''
+        Docstring for build_vocab
+        
+        :param self: Description
+        '''
+
+        token_list = self.tokenize(user_input)
+
+        tokens = set()
+
+        for token in token_list:
+            tokens.add(token)
+        
+        self.vocab = {"[PAD]": 0, "[UNK]": 1}
+
+        for token in tokens:
+            if token not in self.vocab:
+                self.vocab[token] = len(self.vocab)
