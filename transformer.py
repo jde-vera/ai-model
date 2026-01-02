842a5895be4fba98d3549537d79baa5291dfabcb
j <jdmichaelryan@gmail.com>
2026-01-02 22:32:55 +0000

    fix: formatting issue

--- transformer.py
+++ transformer.py
@@ -2,10 +2,12 @@ import re
 
 class Transformer:
     
-    '''
-    the tokenize() take user input (string) and makes use of re
-    '''
+    
     def tokenize(self, user_input):
+        '''
+        the tokenize() take user input (string) and makes use of re
+        '''
+        
         regex = re.compile(
             r"(?:[A-Za-z]+(?:[-'][A-Za-z]+)*) | (?:\d+(?:\.\d+)?) | (?:[^\w\s])", re.VERBOSE)
 
