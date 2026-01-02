c61cd06b39070a731a97009f3f9c223de4878180
j <jdmichaelryan@gmail.com>
2026-01-02 22:13:16 +0000

    func: tokenize(self, user_input) -> mini tokenizer

--- transformer.py
+++ transformer.py
@@ -1,1 +1,12 @@
 import re
+
+class Transformer:
+    
+    '''
+    the tokenize() take user input (string) and makes use of re
+    '''
+    def tokenize(self, user_input):
+        regex = re.compile(
+            r"(?:[A-Za-z]+(?:[-'][A-Za-z]+)*) | (?:\d+(?:\.\d+)?) | (?:[^\w\s])", re.VERBOSE)
+
+        return regex.findall(user_input)
