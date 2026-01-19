5124e1a5f960fd9a47be761cbddedcfa4bbd6a57
j <jdmichaelryan@gmail.com>
2026-01-19 23:59:39 +0000

    docs + fix: fully imp. attention.py

--- attention.py
+++ attention.py
@@ -2,6 +2,34 @@ import math
 import torch
 class SelfAttention(torch.nn.Module):
     def __init__(self, embed_dim):
-        pass
-    def forward(self, attention_mask):
-        pass
+        super().__init__() #thread the constructors together
+        # three matrices which give a different view of the same token
+        self.embed_dim = embed_dim
+        # create a matrice [embed_dim x embed_dim]
+        self.W_q = torch.nn.Linear(embed_dim, embed_dim, bias=False)
+        self.W_k = torch.nn.Linear(embed_dim, embed_dim, bias=False)
+        self.W_v = torch.nn.Linear(embed_dim, embed_dim, bias=False)
+    def forward(self, token_embed, attention_mask):
+        # seq_len is the number of rows
+        seq_len = token_embed.shape[0]
+
+        # then create matrices
+        Q = self.W_q(token_embed)
+        K = self.W_k(token_embed)
+        V = self.W_v(token_embed)
+
+        # calculate attention score
+        scores = (Q @ K.T) / math.sqrt(self.embed_dim)
+
+        # casual mask
+        causal = torch.triu(torch.ones(seq_len, seq_len, device=token_embed.device), diagonal=1).bool()
+        scores = scores.masked_fill(causal, float("-inf"))
+
+        # padding mask
+        if attention_mask is not None:
+            key_mask = (attention_mask == 0).unsqueeze(0)
+            scores = scores.masked_fill(key_mask, float("-inf"))
+        
+        attn = torch.softmax(scores, dim=-1)
+        out = attn @ V
+        return out, attn
--- instance.py
+++ instance.py
@@ -1,4 +1,6 @@
+import torch
 from transformer import Transformer
+from attention import SelfAttention
 
 if __name__ == '__main__':
     transformer = Transformer()
@@ -6,11 +8,19 @@ if __name__ == '__main__':
     transformer.build_vocab(usr_input)
     ids = transformer.get_ids()
     print(ids) # this is to check if the new local variable imp. works
-    transformer.pad_and_mask(ids, 5)
+    id_pad, attention_mask = transformer.pad_and_mask(ids, 5)
 
     # you need to assign the self.embedding variable with an embedding object in order to embed token ids
     transformer.build_embeddings(16)
-    token_vecs = transformer.embed(ids)
+    token_vecs = transformer.embed(id_pad) # create dense vectors for tokens
+    transformer.build_positional_embeddings(max_len=5, embed_dim=16)
+    token_embed = transformer.add_positions(token_vecs) # create embeddings for positions and add to token vectors
+    
+    # ===self attention layer===
+    attention =  SelfAttention(16)
+    mask = torch.tensor(attention_mask, dtype=torch.long)
+    out, attn = attention(token_embed, mask)
 
-    transformer.build_positional_embeddings(max_len=16, embed_dim=16)
-    print(transformer.add_positions(token_vecs))
+    print("token_embed shape:", token_embed.shape)
+    print("out shape:", out.shape)
+    print("attn shape:", attn.shape)
